0891442f6ddf605aeb4fc1aa1c4f8080af728a31
Klaas de Vries <klaasjacobdevries@gmail.com>
2020-06-10 22:10:01 +0100

    adding benchmark

---
+++ examples/benchmarks.cc
@@ -0,0 +1,19 @@
+#include <benchmark/benchmark.h>
+#include <uuid4.h>
+
+namespace uuid4 {
+namespace  {
+
+void bm_generate(benchmark::State& state)
+{
+  for (auto _ : state)
+    make_uuid4();
+}
+
+BENCHMARK(bm_generate);
+
+}
+}
+
+
+BENCHMARK_MAIN();
